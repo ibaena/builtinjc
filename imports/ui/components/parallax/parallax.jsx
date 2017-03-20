0e75e953686faa9b44c233d31a59631160bb4167
Ivan <ivan.baena04@gmail.com>
2017-03-19 22:07:06 -0400

    secondary content added

--- imports/ui/components/parallax/parallax.jsx
+++ imports/ui/components/parallax/parallax.jsx
@@ -22,7 +22,11 @@ export default class Parallax extends Component {
         'transition':'all .4s ease-in-out'
       })
     },100);
-    Scrollbar.initAll();
+    Scrollbar.initAll({
+      'speed':'1.5',
+      'overscrollEffect':'bounce',
+      'damping':'.05'
+    });
   }
 
 
@@ -45,7 +49,25 @@ export default class Parallax extends Component {
             <p className="about-header-sm">View my profile and connect with me on LinkedIn.</p>
             <p className="about-link"><a href="https://github.com/ibaena" target="_blank" className="contact-link special-fx">View LinkedIn Profile</a></p>
           </div>
-          <div className="secondary col-sm-12 col-md-12">Second</div>
+          <div className="secondary col-sm-12 col-md-12">
+            <div className="col-sm-12 col-md-6 add-p-top" >
+                <p className="about-header">What I can do.</p>
+            </div>
+            <div className="col-sm-12 col-md-6 add-p-top-b" >
+              <div className="alphas">
+                <label>Web</label>
+                <p className="alpha-text">reat experiences occur when relevance, insight, and vision meet. Working side by side with our clients, and based on a deep and thorough understanding of their business and challenges, we help them get perspective, take action and be successful in an ever-changing world.</p>
+              </div>
+              <div className="alphas">
+                <label>Design</label>
+                <p className="alpha-text">reat experiences occur when relevance, insight, and vision meet. Working side by side with our clients, and based on a deep and thorough understanding of their business and challenges, we help them get perspective, take action and be successful in an ever-changing world.</p>
+              </div>
+              <div className="alphas">
+                <label>Content</label>
+                <p className="alpha-text">reat experiences occur when relevance, insight, and vision meet. Working side by side with our clients, and based on a deep and thorough understanding of their business and challenges, we help them get perspective, take action and be successful in an ever-changing world.</p>
+              </div>
+            </div>
+          </div>
         </div>
 
       </div>
