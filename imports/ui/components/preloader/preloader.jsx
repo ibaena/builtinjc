0d334064cec97777fc2ad7d2065a614cf8074d29
Ivan <ivan.baena04@gmail.com>
2017-04-02 16:33:15 -0400

    preloader 3 col tween added

--- imports/ui/components/preloader/preloader.jsx
+++ imports/ui/components/preloader/preloader.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import {FlowRouter} from 'meteor/kadira:flow-router';
+import {TweenMax, Power2, TimelineLite} from "gsap";
 
+import SlideA from '../slides/slideA';
 // Intro component
 export default class Preloader extends Component {
   // Note: In ES6, constructor() === componentWillMount() in React ES5
@@ -14,11 +16,11 @@ export default class Preloader extends Component {
       $('.preload-text').addClass('pre-fade-up');
     },600)
     setTimeout(function(){
-      $('#preloader').addClass('pre-fade');
+      TweenMax.to(".preload-box", .4, {width:'0%', ease:Cubic.easeInOut});
     },3000)
     setTimeout(function(){
       FlowRouter.go('/cases');
-    },4200)
+    },3300)
   }
   centerBox(){
     $(window).resize(function(){
@@ -35,10 +37,10 @@ export default class Preloader extends Component {
   render() {
     return (
       <div id="preloader" className="container-fluid">
-        <div className="col-sm-12 col-md-6" id="preload-box">
-          <p className="preload-text">Ivan Baena</p>
-          <p className="preload-text">Web Developer</p>
-        </div>
+        <div className="col-sm-12 col-md-4 preload-box" id=""></div>
+        <div className="col-sm-12 col-md-4 preload-box" id=""></div>
+        <div className="col-sm-12 col-md-4 preload-box" id=""></div>
+        <SlideA />
       </div>
     );
   }
