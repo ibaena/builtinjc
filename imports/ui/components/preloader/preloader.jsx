3182f92ed09a03832aff1e3606ac81996420752e
Ivan <ivan.baena04@gmail.com>
2017-04-02 17:38:20 -0400

    food break

--- imports/ui/components/preloader/preloader.jsx
+++ imports/ui/components/preloader/preloader.jsx
@@ -13,14 +13,16 @@ export default class Preloader extends Component {
   componentDidMount(){
     this.centerBox();
     setTimeout(function(){
-      $('.preload-text').addClass('pre-fade-up');
+      //$('.preload-text').addClass('pre-fade-up');
+      TweenMax.staggerTo(".preload-letter", .4, {top:'25%', ease:Cubic.easeInOut, opacity:1,}, .1);
     },600)
     setTimeout(function(){
-      TweenMax.to(".preload-box", .4, {width:'0%', ease:Cubic.easeInOut});
-    },3000)
+      TweenMax.to(".preload-box", .6, {width:'0%', ease:Cubic.easeInOut});
+      TweenMax.to(".preload-letter", .4, {left:'25%', ease:Cubic.easeInOut, opacity:0});
+    },4500)
     setTimeout(function(){
       FlowRouter.go('/cases');
-    },3300)
+    },5000)
   }
   centerBox(){
     $(window).resize(function(){
@@ -37,6 +39,32 @@ export default class Preloader extends Component {
   render() {
     return (
       <div id="preloader" className="container-fluid">
+        <div className="preload-text">
+          <span className="preload-letter">I</span>
+          <span className="preload-letter">V</span>
+          <span className="preload-letter">A</span>
+          <span className="preload-letter">N</span>
+          <span className="preload-letter"> </span>
+          <span className="preload-letter">B</span>
+          <span className="preload-letter">A</span>
+          <span className="preload-letter">E</span>
+          <span className="preload-letter">N</span>
+          <span className="preload-letter">A</span>
+          <br />
+          <span className="preload-letter">W</span>
+          <span className="preload-letter">E</span>
+          <span className="preload-letter">B</span>
+          <span className="preload-letter"> </span>
+          <span className="preload-letter">D</span>
+          <span className="preload-letter">E</span>
+          <span className="preload-letter">V</span>
+          <span className="preload-letter">E</span>
+          <span className="preload-letter">L</span>
+          <span className="preload-letter">O</span>
+          <span className="preload-letter">P</span>
+          <span className="preload-letter">E</span>
+          <span className="preload-letter">R</span>
+        </div>
         <div className="col-sm-12 col-md-4 preload-box" id=""></div>
         <div className="col-sm-12 col-md-4 preload-box" id=""></div>
         <div className="col-sm-12 col-md-4 preload-box" id=""></div>
